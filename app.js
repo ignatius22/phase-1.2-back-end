bc40c7f34f5725f9662d90d857065cb2a9cb3f22
ignatius22 <igee54077@gmail.com>
2021-01-21 11:59:58 +0100

    add node-fetch

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const app = express()
 
 
 app.get('/', (req, res) => {
-	res.json({name: 'express app'})
+    var url = 'https://api.darksky.net/forecast/<API KEY>/37.8267,-122.4233';
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        res.send({ data });
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+
+    
 })
 
 const port = 3000; 
