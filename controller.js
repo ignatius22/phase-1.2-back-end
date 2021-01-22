51f2e6764e22af4d0c360c2aeb922469387a277a
ignatius22 <igee54077@gmail.com>
2021-01-22 13:32:31 +0100

    trim incomming query

--- controller.js
+++ controller.js
@@ -2,17 +2,46 @@ const fetch = require('node-fetch');
 
 
 exports.getCurrency = ((req, res) => {
-	var url = 'https://api.exchangeratesapi.io/latest';
+
+    let { base, currency } = req.query;
+
+  base = typeof base !== undefined ? base.trim().toUpperCase() : '';
+
+
+	let url = `https://api.exchangeratesapi.io/latest?base=${base}`;
 	
   fetch(url)
     .then((res) => res.json())
 		.then((data) => {
 			
-		  res.send(data)
+		  currency =
+        typeof currency !== undefined ? currency.trim().split(',') : [];
+           let rates = {};
+           currency.forEach((rate) => {
+             totalRate = rate.toUpperCase() || undefined;
+             if (rate.length >= 1) {
+               rates[totalRate] =
+                 data.rates[totalRate] || 'we could not find the following query';
+             } else {
+               rates[totalRate] = data.rates[totalRate];
+             }
+           });
+         const requestedData = {
+           results: {
+             base: base,
+             date: data.date,
+             rates: rates,
+           },
+         };
+         
+         return res.send(requestedData).status(200);
+      
     })
     .catch((err) => {
       res.send(err);
 		});
 	 
 });
+
+
 
